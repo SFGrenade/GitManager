759db6ea74a5d2cefb861cb71123901b61c5345d
SFGrenade <25555417+SFGrenade@users.noreply.github.com>
2026-08-29 15:24:36 +0200

    add define for libgit types and aquiring/freeing them

--- include/utils.hpp
+++ include/utils.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 // C++ headers
+#include <memory>
 #include <string>
 
 template < class T, size_t N >
@@ -10,3 +11,17 @@ constexpr size_t arraySize( T ( & )[N] ) {
 
 void toUpper( std::string& str );
 void toLower( std::string& str );
+
+#define RAW_PTR_TO_SHARED_PTR( type, name, deleteMethod, createMethod, ... ) \
+  std::shared_ptr< type > name;                                              \
+  {                                                                          \
+    type* r_##name = nullptr;                                                \
+    if( int error = createMethod( __VA_ARGS__ ); error < 0 ) {               \
+      PrintGitError( #createMethod, error );                                 \
+      return;                                                                \
+    }                                                                        \
+    name = std::shared_ptr< type >( r_##name, []( type* p ) {                \
+      if( p )                                                                \
+        deleteMethod( p );                                                   \
+    } );                                                                     \
+  }
